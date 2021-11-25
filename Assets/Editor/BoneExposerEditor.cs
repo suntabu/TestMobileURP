488ebf68d3e86d9317255a11c44d7490f5209941
gouzhun <gouzhun@libii.com>
2021-11-25 17:01:01 +0800

    add content

---
+++ Assets/Editor/BoneExposerEditor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+
+namespace UnityToolKit
+{
+    [CustomEditor(typeof(BoneExposer))]
+    public class BoneExposerEditor : Editor
+    {
+        private Transform copyRoot;
+        private SkinnedMeshRenderer src;
+
+        public override void OnInspectorGUI()
+        {
+            // Get the renderer from the target object
+            var renderer = (target as BoneExposer).gameObject.GetComponent<SkinnedMeshRenderer>();
+
+            // If there is no renderer, we can't do anything
+            if (!renderer)
+            {
+                EditorGUILayout.HelpBox("SortingLayerExposed must be added to a game object that has a renderer.",
+                    MessageType.Error);
+                return;
+            }
+
+            var bones = renderer.bones;
+            for (var index = 0; index < bones.Length; index++)
+            {
+                Transform bone = bones[index];
+                bones[index] = EditorGUILayout.ObjectField("bone " + index, bone, typeof(Transform), true) as Transform;
+            }
+
+            copyRoot = EditorGUILayout.ObjectField("From root", copyRoot, typeof(Transform), true) as Transform;
+            if (copyRoot)
+            {
+                src = EditorGUILayout.ObjectField("Src Bones", src, typeof(SkinnedMeshRenderer), true) as
+                    SkinnedMeshRenderer;
+
+                var mybones = new List<Transform>();
+                if (src)
+                {
+                    for (int i = 0; i < src.bones.Length; i++)
+                    {
+                        var bone = src.bones[i];
+                        if (bone)
+                            mybones.Add(copyRoot.GetComponentsInChildren<Transform>()
+                                .FirstOrDefault(t => t.name == bone.name));
+                    }
+                    renderer.bones = mybones.ToArray();
+                    copyRoot = null;
+                }
+
+              
+            }
+        }
+    }
+}
---
+++ Assets/TouchRotate.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchRotate : MonoBehaviour
+{
+    private Vector3 lastPos, delta;
+    private bool isOnModel, isDown;
+    private Transform target;
+
+    void Update()
+    {
+        if (isDown && Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out var hitInfo))
+        {
+            delta = Input.mousePosition - lastPos;
+            lastPos = Input.mousePosition;
+            if (hitInfo.collider.transform)
+                target = hitInfo.collider.transform;
+            else
+            {
+                target = null;
+            }
+        }
+        else
+        {
+            target = null;
+        }
+
+        if (target)
+        {
+            target.localRotation *= Quaternion.Euler(0, delta.x, 0);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastPos = Input.mousePosition;
+            isDown = true;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDown = false;
+        }
+    }
+}
